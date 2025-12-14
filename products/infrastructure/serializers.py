1fd99a07562d03572ad1aad435e7e6400a1bfa62
Rafael Acevedo <rafacevedo9@gmail.com>
2025-12-14 12:48:37 -0500

    Delete all

--- products/infrastructure/serializers.py
+++
@@ -1,9 +0,0 @@
-# products/infrastructure/serializers.py
-
-from rest_framework import serializers
-from .models import Producto
-
-class ProductoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Producto
-        fields = ['id', 'nombre', 'descripcion', 'precio']
